eacec99da1d1d0371294cbd2e6e7a58e04162f2c
PrajwalPawar789 <prajwalrpawar2001@gmail.com>
2023-12-29 18:17:47 +0530

    new app

---
+++ src/Redux/slices.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const slices = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default slices
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="flex flex-row ">
+        Home
+        Cart
+    </div>
+  )
+}
+
+export default Navbar
